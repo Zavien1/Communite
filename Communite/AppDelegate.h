7372d3d3b7ea2c825558e5c57322b649c8d84965
Zavien Sibilia <zavien@fb.com>
2021-07-12 14:33:21 -0700

    Initial Commit

---
+++ Communite/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Communite
+//
+//  Created by Zavien Sibilia on 7/12/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Communite/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Communite
+//
+//  Created by Zavien Sibilia on 7/12/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
