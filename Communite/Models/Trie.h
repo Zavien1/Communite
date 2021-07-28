981c15ecebbe6a622f9363f552dafa95056a6690
Zavien Sibilia <zavien@fb.com>
2021-07-27 17:33:18 -0700

    connect UI elements to display data from database

---
+++ Communite/Models/Trie.h
@@ -0,0 +1,16 @@
+//
+//  Trie.h
+//  Communite
+//
+//  Created by Zavien Sibilia on 7/27/21.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Trie : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Communite/Models/Trie.m
@@ -0,0 +1,12 @@
+//
+//  Trie.m
+//  Communite
+//
+//  Created by Zavien Sibilia on 7/27/21.
+//
+
+#import "Trie.h"
+
+@implementation Trie
+
+@end
